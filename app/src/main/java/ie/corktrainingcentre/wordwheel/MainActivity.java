0fbea157dfdf8aaa277b234d92157465b2613c3f
Fergal <fitz.fergal@gmail.com>
2015-06-11 11:35:30 +0100

    I wonder if this will work

--- app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
+++ app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
@@ -30,9 +30,11 @@ public class MainActivity extends Activity {
         textView2 = (TextView) findViewById(R.id.textView2);
         userInputEditText = (EditText) findViewById(R.id.editText);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        String[] lengthsOfWord = {"9", "10","11", "12"};
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.word_length, android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
+                android.R.layout.simple_expandable_list_item_1,lengthsOfWord);
         // Specify the layout to use when the list of choices appears
        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
@@ -44,25 +46,5 @@ public class MainActivity extends Activity {
         textView2.setText(String.valueOf(wordWheel.getCentreChar()));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }

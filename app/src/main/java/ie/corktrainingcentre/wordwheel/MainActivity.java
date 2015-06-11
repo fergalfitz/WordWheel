303d3462d10646c6a62e934d934aeab1ba73cf37
Fergal <fitz.fergal@gmail.com>
2015-06-11 15:39:49 +0100

    Nearly finished a day at college

--- app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
+++ app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -22,26 +23,22 @@ public class MainActivity extends Activity {
     TextView textView;
     TextView textView2;
     EditText userInputEditText;
+    Button checkWordButton;
+    Spinner spinner;
+    WordWheel wordWheel;
+    final String[] lengthsOfWord = {"9", "10","11", "12"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView2 = (TextView) findViewById(R.id.textView2);
-        userInputEditText = (EditText) findViewById(R.id.editText);
-        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        final WordWheel wordWheel = new WordWheel(12);
+        intializeObjectsFromXML_Resourses();
 
-        final String[] lengthsOfWord = {"9", "10","11", "12"};
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
-                android.R.layout.simple_expandable_list_item_1,lengthsOfWord);
-        // Specify the layout to use when the list of choices appears
-       adapter.setDropDownViewResource(android.R.layout.sp);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+
+
+
+
         // Add a on selected listener to the spinner
         /*spinner.setOnClickListener(new AdapterView.OnItemSelectedListener(){
 
@@ -61,11 +58,29 @@ public class MainActivity extends Activity {
 
         });*/
 
-        wordWheel.setWordlenght(Integer.valueOf((String)spinner.getSelectedItem()));
+        wordWheel.setWordlenght(Integer.valueOf((String) spinner.getSelectedItem()));
         textView.setText(wordWheel.getWord());
         //textView2.setText(String.valueOf(wordWheel.getCentreChar()));
         textView2.setText(wordWheel.scrambledWord(wordWheel.getWord()));
 
+
+    }
+    private void intializeObjectsFromXML_Resourses()
+    {
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        userInputEditText = (EditText) findViewById(R.id.editText);
+        checkWordButton = (Button) findViewById(R.id.checkWordButton);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
+                android.R.layout.simple_expandable_list_item_1,lengthsOfWord);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        // Must be done after spinner has been intialized
+        wordWheel = new WordWheel(Integer.valueOf((String) spinner.getSelectedItem()));
 
     }
 

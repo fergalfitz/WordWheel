07ea25a1b90b94b429c7fdf1507f181bfa3c427f
Fergal <fitz.fergal@gmail.com>
2015-06-11 11:25:34 +0100

    I wonder if this will work

--- app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
+++ app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
@@ -32,9 +32,9 @@ public class MainActivity extends Activity {
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.word_length, android.R.layout.simple_spinner_item);
+                R.array.word_length, android.R.layout.simple_spinner_dropdown_item);
         // Specify the layout to use when the list of choices appears
-   //     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
 
         spinner.setAdapter(adapter);

8728efbfecc0111369941f7fa1db1af1e34293d6
fergalfitz <fitz.fergal@gmail.com>
2015-06-14 21:14:32 +0100

    Finished Todo: Tidy up gui, change character size to dpi,

--- app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
+++ app/src/main/java/ie/corktrainingcentre/wordwheel/MainActivity.java
@@ -1,19 +1,30 @@
 package ie.corktrainingcentre.wordwheel;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
@@ -30,6 +41,7 @@ public class MainActivity extends Activity {
     Spinner spinner;
     WordWheel wordWheel;
     TextView resultsTextView;
+    LinearLayout wordWheelLinearLayout;
     final String[] lengthsOfWord = {"9", "10","11", "12"};
 
     @Override
@@ -61,6 +73,12 @@ public class MainActivity extends Activity {
                             Toast.LENGTH_SHORT).show();
                     userInputEditText.setText("");
                 }
+                else if (!userInput.contains(String.valueOf(wordWheel.getCentreChar()))){
+                    Toast.makeText(getApplicationContext(), "Guess does not contain \"" +
+                                    wordWheel.getCentreChar() + "\"",
+                            Toast.LENGTH_SHORT).show();
+
+                }
                 else {
                     Result result = wordWheel.checkWord(userInput);
                     resultsTextView.append("\n" + result.getMessage());
@@ -88,7 +106,10 @@ public class MainActivity extends Activity {
         textView.setText(wordWheel.getWord());
         //textView2.setText(String.valueOf(wordWheel.getCentreChar()));
         textView2.setText(wordWheel.scrambledWord(wordWheel.getWord()));
+        textView2.append("--" + wordWheel.getCentreChar());
         resultsTextView.setText("");
+        wordWheelLinearLayout.removeAllViews();
+        wordWheelLinearLayout.addView(new wordWheelView(this));
     }
     private void intializeObjectsFromXML_Resourses()
     {
@@ -100,6 +121,17 @@ public class MainActivity extends Activity {
         solutionsButton = (Button) findViewById(R.id.solutionsButton);
         spinner = (Spinner) findViewById(R.id.spinner);
         resultsTextView = (TextView) findViewById(R.id.resultsTextView);
+        wordWheelLinearLayout = (LinearLayout) findViewById(R.id.wordWheelLinearLayout);
+
+//        set the height of wordWheelLinearLayout to half of the screen
+        ViewGroup.LayoutParams params = wordWheelLinearLayout.getLayoutParams();
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        params.height = height /3;
+        wordWheelLinearLayout.addView(new wordWheelView(this));
+
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),
                 android.R.layout.simple_expandable_list_item_1,lengthsOfWord);
@@ -119,7 +151,7 @@ public class MainActivity extends Activity {
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView,
                                        int position, long id) {
 
-                //wordWheel.setWordlenght(Integer.valueOf((String)spinner.getSelectedItem()));
+                refreshWordWheel();
             }
 
             @Override
@@ -130,5 +162,72 @@ public class MainActivity extends Activity {
 
         });*/
 
+    }
+
+    public class wordWheelView extends View {
+
+        public wordWheelView(Context context) {
+            super(context);
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // TODO Auto-generated method stub
+            super.onDraw(canvas);
+
+            int x = getWidth();
+            int y = getHeight();
+
+            Paint paint = new Paint();
+            paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(2);
+
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawPaint(paint);
+//            paint.
+            canvas.drawCircle(x / 2, y / 2, y / 2, paint);
+            paint.setColor(Color.RED);
+            paint.setStrokeWidth(4);
+            canvas.drawCircle(x / 2, y / 2, y / 8, paint);
+
+
+//            draw the center character
+            int offsetForChar = 10;
+
+            paint.setColor(Color.BLACK);
+            paint.setTextSize(40);
+            paint.setStrokeWidth(2);
+            canvas.drawText(String.valueOf(wordWheel.getCentreChar()), (x / 2) -offsetForChar ,
+                    (y / 2) + offsetForChar, paint);
+
+//              remove the center character from the letters to be displayed on the circle
+            StringBuffer scrambledWord = new StringBuffer(wordWheel.scrambledWord(wordWheel.getWord()));
+            scrambledWord.deleteCharAt(scrambledWord.indexOf(String.valueOf(wordWheel.getCentreChar())));
+
+//            final int totalAngle = 360;
+//            int angleIncrements = 360 / scrambledWord.length();
+            double slice = 2 * Math.PI / scrambledWord.length();
+            int radius = y/ 3;
+            int xPosition = 0;
+            int yPosition = 0;
+
+            for(int i = 0; i < scrambledWord.length() ; i++)
+            {
+                double angle = slice * i;
+                xPosition = (int)(radius* Math.cos(angle) + (x / 2) - offsetForChar);
+                yPosition = (int)(radius* Math.sin(angle)+ (y / 2) +offsetForChar);
+                char c = scrambledWord.charAt(i);
+                String letter = String.valueOf(c);
+
+                canvas.drawText(letter, xPosition,yPosition, paint);
+
+            }
+
+
+
+
+
+        }
     }
 }
